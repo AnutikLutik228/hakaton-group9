58a2be8ba8eedda1b4e84eb9ea4125add4227652
dosmils <baratova_m_d@mail.ru>
2026-05-31 22:30:12 +0300

    Исправление ошибки в SRC (было SCR -> стало SRC). Обновление requirements.txt

--- scr/generate_report.py
+++
@@ -1,7 +0,0 @@
-def generate_report(stats):
-    with open("report.txt", "w", encoding="utf-8") as file:
-        file.write("Report\n")
-        file.write("------\n")
-
-        for category in stats:
-            file.write(category + ": " + str(stats[category]) + "\n")
